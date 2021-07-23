9056d18ddf1823ce08b8117e7a9ef0dc6d4831c9
dvl1415 <dhavalpatidar145@gmail.com>
2021-07-22 21:59:53 -0400

    Added the Interpolation and Property Binding

--- angular-feature-app/src/app/app.component.ts
+++ angular-feature-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // selector: 'app-root',
+  // selector: '[app-root]',
+  selector: '.app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-feature-app';
+  interPolation = 'One-Way Data Binding';
+  // title = 'angular-feature-app';
+  firstName = 'Dhaval';
+  lastName = 'Patidar';
+  person = {name: 'DVl'}
+
+  propertyBinding = "Property Binding"
+  innerText = "This Value is coming from the component"
+  isDisabled = true;
+  url = "https://angular.io/cli"
 }
--- angular-feature-app/src/app/components/student-test/student-test.component.ts
+++ angular-feature-app/src/app/components/student-test/student-test.component.ts
@@ -2,8 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student-test',
-  templateUrl: './student-test.component.html',
+  template: `
+  <div class = "container">
+     <h1> Student Name : {{name}}</h1>
+  </div>
+ 
+ `,
+  // templateUrl: './student-test.component.html',
   styleUrls: ['./student-test.component.css']
+  // styles: [
+  //   'h1 {color:red}'
+  // ]
 })
 export class StudentTestComponent implements OnInit {
 
